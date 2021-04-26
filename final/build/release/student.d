e90c1a506fe07158bad06859342584fd61c09c61
Ronan-Kelley <ronankelley2@gmail.com>
2021-04-26 12:19:16 -0400

    cleanup

--- final/build/release/student.d
+++
@@ -1,3 +0,0 @@
-build/release/student.o: src/student.c include/student.h
-
-include/student.h:
--- final/build/release/test.d
+++
@@ -1,8 +0,0 @@
-build/release/test.o: src/test.c include/test.h include/student.h \
- include/student.h
-
-include/test.h:
-
-include/student.h:
-
-include/student.h:
